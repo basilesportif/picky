2c7184632d28b0fa0989670b1a5a4bb377b0cf3a
timlucmiptev <info@timlucmiptev.space>
2020-08-03 11:24:10 +0300

    files added

---
+++ README.md
@@ -0,0 +1,2 @@
+# picky
+See who is active or not in your chats. Keep control as Urbit expands.
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "usage: sync.sh URBIT_PIER_DIRECTORY"
+    exit;
+fi
+
+cp -r app/* $1/app/
+cp -r mar/* $1/mar/
+cp -r sur/* $1/sur/
+
+
