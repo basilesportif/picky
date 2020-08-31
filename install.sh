c61e70da7a3afc012c6e74807a5a762f173bce19
timlucmiptev <info@timlucmiptev.space>
2020-08-31 11:46:29 +0300

    dir cleanup

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-if [ -z "$1" ]
-then
-    echo "usage: sync.sh URBIT_PIER_DIRECTORY"
-    exit;
+usage() { echo "Usage: $0 URBIT_PIER_DIRECTORY [-w (watch code and copy to pier)]" 1>&2; exit 1; }
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 2
 fi
 
-echo "Watching for changes to copy to ${1}..."
-while [ 0 ]
-do
-    sleep 0.1
-    rsync -r --exclude '.*' --exclude '*.sh' --exclude '*.md' * $1
+while getopts "w" opt; do
+    case ${opt} in
+        w) WATCH_MODE="true"
+           ;;
+        *) usage
+           ;;
+    esac
 done
+
+if [ -z "$WATCH_MODE"]; then
+    echo "Installed %picky"
+    rsync -r --exclude '.*' --exclude '*.sh' --exclude '*.md' * $1/
+else
+   echo "Watching for changes to copy to ${1}..."
+   while [ 0 ]
+   do
+    sleep 0.8
+    rsync -r --exclude '.*' --exclude '*.sh' --exclude '*.md' * $1/
+   done
+fi
